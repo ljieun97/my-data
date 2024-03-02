3c4e85a31f4bec73e38fb9c30b89874cba1dbe25
ljieun <wldms8196@gmail.com>
2024-03-02 14:27:50 +0900

    feat: day js

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
+import dayjs from 'dayjs'
 const API_KEY = process.env.API_KEY_TMDB
-const getToday = () => {
-  const date = new Date()
-  const year = date.getFullYear()
-  const month = ('0' + (date.getMonth() + 1)).slice(-2)
-  const day = ('0' + (date.getDate())).slice(-2)
-  return `${year}-${month}-${day}`
-}
+const Today = dayjs().format('YYYY-MM-DD')
 const nextConfig = {
   reactStrictMode: false,
   images: {
@@ -16,7 +11,7 @@ const nextConfig = {
     return [
       {
         source: "/api/tm-todays",
-        destination: `https://api.themoviedb.org/3/discover/tv?air_date.gte=${getToday()}&air_date.lte=${getToday()}&language=ko&watch_region=KR&with_watch_providers=8|9|96|97|337|350|356&api_key=${API_KEY}`
+        destination: `https://api.themoviedb.org/3/discover/tv?air_date.gte=${Today}&air_date.lte=${Today}&language=ko&watch_region=KR&with_watch_providers=8|9|96|97|337|350|356&api_key=${API_KEY}`
       },
       {
         source: "/api/tm-search/:id",

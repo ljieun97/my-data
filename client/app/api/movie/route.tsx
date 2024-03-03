aa3d3d116c747b8573a7fdf240cee34fca90972f
ljieun <wldms8196@gmail.com>
2024-03-03 23:15:12 +0900

    update: 홈에서 재등록시 별점만 변경 날짜는 그대로

--- client/app/api/movie/route.tsx
+++ client/app/api/movie/route.tsx
@@ -21,25 +21,28 @@ const GET = async () => {
 const POST = async (req: NextRequest) => {
 	const { movie, rating } = await req.json()
 	let title
-  if(movie.title) {
+	if (movie.title) {
 		title = `${movie.title} (${movie.release_date.split("-")[0]})`
 	} else {
 		title = `${movie.name} (${movie.first_air_date.split("-")[0]})`
 	}
 	const image = `https://www.themoviedb.org/t/p/w1280${movie.poster_path}`
 	const today = dayjs().format('YYYY-MM-DD')
-	
+
 	try {
 		const db = await connectMongo()
 		await db
 			.collection("my_movies")
-			.updateOne({ title }, {$set: {
-				userId: 1,
-				title,
-				info: {id: movie.id, image, genre_ids: movie.genre_ids, media_type: movie.media_type},
-				date: today,
-				rating
-			}}, { upsert: true })
+			.updateOne({ title },
+				{
+					$set: {
+						userId: 1,
+						title,
+						info: { id: movie.id, image, genre_ids: movie.genre_ids, media_type: movie.media_type },
+						rating
+					},
+					$setOnInsert: { date: today }
+				}, { upsert: true })
 		return NextResponse.json({ message: "success /movie POST" })
 	} catch (e) {
 		console.log(e)

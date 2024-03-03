7d8a9878bd185bee320e672943a33dc8561e9581
ljieun <wldms8196@gmail.com>
2024-03-03 18:15:43 +0900

    update: mongodb user and pass

--- client/lib/mongo/mongodb.ts
+++ client/lib/mongo/mongodb.ts
@@ -1,34 +1,14 @@
-// import { MongoClient } from "mongodb"
-
-// const MONGODB_URL = process.env.MONGODB_URL as string
-// const MONGODB_DB = process.env.MONGODB_DB as string
-
-// declare let _mongo: any
-// declare let _db: any
-
-// const connectMongo = async (): Promise<any> => {
-//   console.log(MONGODB_URL)
-//   if(_mongo && _db) return _db
-//   _mongo = await MongoClient.connect(MONGODB_URL)
-//   console.log(_mongo)
-//   _db =  _mongo.db(MONGODB_DB)
-// 	const connectDB = _db
-//   return connectDB
-// }
-
-// export default connectMongo
-
 import { MongoClient } from "mongodb"
 
-const MONGODB_URL = process.env.MONGODB_URL || ''
-const MONGODB_DB = process.env.MONGODB_DB || ''
+const MONGODB_URI = process.env.MONGODB_URI || ''
+const MONGODB_DB = process.env.MONGODB_DB
 
 let cachedClient: any
 let cachedDb: any
 
 const connectMongo = async (): Promise<any> => {
   if (cachedClient && cachedDb) return cachedDb
-  const client = await MongoClient.connect(MONGODB_URL)
+  const client = await MongoClient.connect(MONGODB_URI)
   const db = client.db(MONGODB_DB)
 	cachedClient = client
 	cachedDb = db

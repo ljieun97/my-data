94e011b2ad0c0af5291468171268aedd90d1968b
ljieun <wldms8196@gmail.com>
2024-04-01 18:34:29 +0900

    test: vercel api endpoint

--- components/movie/like-movies.tsx
+++ components/movie/like-movies.tsx
@@ -8,8 +8,13 @@ export default function LikeMovies() {
   const [movies, setMovies] = useState([])
   useEffect(() => {
     (async () => {
-      const movies = await GetMovies()
-      setMovies(movies)
+      // const movies = await GetMovies()
+      // setMovies(movies)
+      const response = await fetch(`/api/movie`, {
+        method: "GET"
+      })
+      const result = await response.json()
+      setMovies(result)
     })()
   }, [])
   return (

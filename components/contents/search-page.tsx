aca77fd4f6fec2a43f3fa97a5e00a09710ce6ac6
ljieun <wldms8196@gmail.com>
2024-05-08 18:18:50 +0900

    update: css

--- components/contents/search-page.tsx
+++ components/contents/search-page.tsx
@@ -11,14 +11,16 @@ export default function SearchPage() {
 
   return (
     <>
-        <Spacer y={16}/>
-      <div className="flex items-center pt-8 pb-4">
-        <Title
-          title={`[${keyword}] 검색결과`}
-          sub={"두 글자 이상 입력 및 띄어쓰기 주의"}
-        />
+      <Spacer y={16} />
+      <div className="px-2 py-6 mx-auto max-w-7xl">
+        <div className="px-4 pt-8 pb-4 flex items-center">
+          <Title
+            title={`[${keyword}] 검색결과`}
+            sub={"두 글자 이상 입력 및 띄어쓰기 주의"}
+          />
+        </div>
+        <SearchList keyword={keyword} />
       </div>
-      <SearchList keyword={keyword} />
     </>
   )
 }

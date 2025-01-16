2ffa3efd04759c126fc1bd73869d70fe804e8032
ljieun <wldms8196@gmail.com>
2025-01-16 18:59:21 +0900

    fix: change params type

--- app/(content)/tv/[id]/page.tsx
+++ app/(content)/tv/[id]/page.tsx
@@ -1,22 +1,29 @@
 import DetailPage from "@/page/detail-page"
 import { getCasts, getDetail, getProviders, getRecommendations, getSimilars, getVideo } from "@/lib/themoviedb/api"
 
-export const dynamic = "force-dynamic"
 export const metadata = {
   title: "시리즈"
 }
 
-export default async function Page({ params }: { params: any }) {
+export default async function Page({ params }: { params: { id: string } }) {
   const { id } = await params
   const type = 'tv'
-  const content = await getDetail(type, id)
-  const video = await getVideo(type, id)
-  const casts = await getCasts(type, id)
-  const rcm = await getRecommendations(type, id)
+  const [content, video, casts, rcm, providers] = await Promise.all([
+    getDetail(type, id),
+    getVideo(type, id),
+    getCasts(type, id),
+    getRecommendations(type, id),
+    getProviders(type, id),
+  ])
   const sim = (rcm?.length > 0 ? rcm : await getSimilars(type, id))
-  const providers = await getProviders(type, id)
 
   return (
-    <DetailPage content={content} casts={casts} sim={sim} providers={providers} videoKey={video?.key} />
+    <DetailPage
+      content={content}
+      casts={casts}
+      sim={sim}
+      providers={providers}
+      videoKey={video?.key}
+    />
   )
 }
